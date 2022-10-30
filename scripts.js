fe6c56a7a748bac5214e136648e4ac441080cd0c
unknown <wallacetashyn@gmail.com>
2022-10-29 23:45:25 -0500

    added the javascript functionality to the project

--- scripts.js
+++ scripts.js
@@ -1,1 +1,15 @@
 /* Add your JavaScript to this file */
+window.onload = function () {
+  document
+    .querySelector(".newsletter form")
+    .addEventListener("submit", function (e) {
+      e.preventDefault();
+      const input = document.querySelector(".form-group input");
+      if (!input.value.trim())
+        return (document.getElementsByClassName("message")[0].innerHTML =
+          "Please enter a valid email address.");
+      document.getElementsByClassName(
+        "message"
+      )[0].innerHTML = `Thank you! Your email address ${input.value.trim()} has been added to our mailing list!`;
+    });
+};
